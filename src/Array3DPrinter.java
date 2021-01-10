8063ecc350e2677ee5f9a2309f65547a74b627c1
Karolina Feret <feret.karolina@gmail.com>
2021-01-10 15:41:56 +0100

    dodanie zmian

--- src/Array3DPrinter.java
+++ src/Array3DPrinter.java
@@ -1,62 +1,74 @@
+import java.sql.SQLOutput;
+
 public class Array3DPrinter {
     public static void main(String[] args) {
 
-        int[][][] arr = {{{1,2},{5}},{{1}},{{1,2,3},{1,1}}};
+        int[][][] arr = {{{1, 2}, {5}}, {{1}}, {{1, 2, 3}, {1, 1}}};
 
-        for (int i = 0; i < arr.length ; i++) {
-            for (int j = 0; j < arr[i].length ; j++) {
-                for (int k = 0; k < arr[i][j].length ; k++) {
-                    System.out.println("arr["+i+"]["+j+"]["+k+"] = " + arr[i][j][k]);
-
-                }
-                
-            }
-            
-        }
-    }
-        public static void main(String[] args) {
-
-        int[][][] arr = {{{1,2},{5}},{{1}},{{1,2,3},{1,1}}};
-
-        for (int i = 0; i < arr.length ; i++) {
-            for (int j = 0; j < arr[i].length ; j++) {
-                for (int k = 0; k < arr[i][j].length ; k++) {
-                    System.out.println("arr["+i+"]["+j+"]["+k+"] = " + arr[i][j][k]);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int k = 0; k < arr[i][j].length; k++) {
+                    System.out.println("arr[" + i + "][" + j + "][" + k + "] = " + arr[i][j][k]);
 
                 }
-                
+
             }
-            
+
         }
     }
-        public static void main(String[] args) {
 
-        int[][][] arr = {{{1,2},{5}},{{1}},{{1,2,3},{1,1}}};
+    public static void main(String[] args) {
 
-        for (int i = 0; i < arr.length ; i++) {
-            for (int j = 0; j < arr[i].length ; j++) {
-                for (int k = 0; k < arr[i][j].length ; k++) {
-                    System.out.println("arr["+i+"]["+j+"]["+k+"] = " + arr[i][j][k]);
+        int[][][] arr = {{{1, 2}, {5}}, {{1}}, {{1, 2, 3}, {1, 1}}};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int k = 0; k < arr[i][j].length; k++) {
+                    System.out.println("arr[" + i + "][" + j + "][" + k + "] = " + arr[i][j][k]);
 
                 }
-                
+
             }
-            
+
         }
     }
-            public static void main(String[] args) {
 
-        int[][][] arr = {{{1,2},{5}},{{1}},{{1,2,3},{1,1}}};
+    public static void main(String[] args) {
 
-        for (int i = 0; i < arr.length ; i++) {
-            for (int j = 0; j < arr[i].length ; j++) {
-                for (int k = 0; k < arr[i][j].length ; k++) {
-                    System.out.println("arr["+i+"]["+j+"]["+k+"] = " + arr[i][j][k]);
+        int[][][] arr = {{{1, 2}, {5}}, {{1}}, {{1, 2, 3}, {1, 1}}};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int k = 0; k < arr[i][j].length; k++) {
+                    System.out.println("arr[" + i + "][" + j + "][" + k + "] = " + arr[i][j][k]);
 
                 }
-                
+
             }
-            
+
         }
     }
+
+    public static void main(String[] args) {
+
+        int[][][] arr = {{{1, 2}, {5}}, {{1}}, {{1, 2, 3}, {1, 1}}};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                for (int k = 0; k < arr[i][j].length; k++) {
+                    System.out.println("arr[" + i + "][" + j + "][" + k + "] = " + arr[i][j][k]);
+
+                }
+
+            }
+
+        }
+    }
+
+}
+
+    public static void main(String[] args) {
+        System.out.println("komentarzjakis");
+    }
+
 }
